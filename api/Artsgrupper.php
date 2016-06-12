c4e42b4e74dc595af53c5b9324bb37fc3ba966f1
davidkonrad <davidkonrad@gmail.com>
2016-06-12 22:47:36 +0200

    API artsgrupper includes rige / kingdom in result; sunsetting bioblitz addon completely

--- api/Artsgrupper.php
+++ api/Artsgrupper.php
@@ -11,26 +11,22 @@ class Artsgrupper extends Base {
 
 	private function run($query) {
 		if ($query=='') {
-			$SQL='select distinct Artsgruppe, Artsgruppe_dk from allearter order by Artsgruppe';
+			$SQL='select distinct Artsgruppe, Artsgruppe_dk, Rige, Rige_dk from allearter order by Artsgruppe';
 		} else {
-			$SQL='select distinct Artsgruppe, Artsgruppe_dk from allearter '.
+			$SQL='select distinct Artsgruppe, Artsgruppe_dk, Rige, Rige_dk from allearter '.
 				'where Artsgruppe like "%'.$query.'%" or Artsgruppe_dk like "%'.$query.'%" '.
 				'order by Artsgruppe';
 		}
 
 		mysql_set_charset('utf8');
 		$result=$this->query($SQL);
-		$JSON='';
-		while ($row = mysql_fetch_assoc($result)) {
-			if (($row['Artsgruppe']!='') || ($row['Artsgruppe_dk']!='')) {
-				if ($JSON!='') $JSON.=', ';
-				$JSON.=$this->rowToJSON($row);
-			}
-		}
-		$JSON='{ "allearter" : ['.$JSON.'] }';
-		echo $JSON;
-	}
 
-}
+		$json = array();
+		while ($row = mysql_fetch_assoc($result)) {
+			$json[] = $row;
+		}
+		$json = array('allearter' => $json);
+		echo json_encode($json);
+	}}
 
 ?>
