e397274c047c7169ef1b7fd95b118d62115e5201
davidkonrad <davidkonrad@gmail.com>
2016-06-12 13:03:04 +0200

    updated API in order to deliver faster lookups

--- api/Arter.php
+++ api/Arter.php
@@ -6,10 +6,22 @@ class Arter extends Base {
 
 	public function __construct($query) {
 		parent::__construct();
+
 		if ($query=='') {
 			$this->getError('Søgetekst ikke defineret');
-		} else {
-			$this->run($query);
+		}
+
+		$type = isset($_GET['type']) ? $_GET['type'] : false;
+		switch($type) {
+			case 'dk' :
+				$this->run_Dk($query);
+				break;
+			case 'lat' :
+				$this->run_Vn($query);
+				break;
+			default :
+				$this->run($query);
+				break;
 		}
 	}
 
@@ -20,18 +32,44 @@ class Arter extends Base {
 			'and Dansk="Ja"';
 
 		mysql_set_charset('utf8');
-
 		$result=$this->query($SQL);
-		
 		$JSON='';
 		while ($row = mysql_fetch_assoc($result)) {
 			if ($JSON!='') $JSON.=', ';
-			//$JSON.='"'.$row['Videnskabeligt_navn'].'"';
 			$JSON.=$this->rowToJSON($row);
 		}
 		$JSON='{ "allearter" : ['.$JSON.'] }';
 		echo $JSON;
 	}
+
+	protected function run_Dk($query) {
+		$SQL='select distinct Videnskabeligt_navn, Dansk_navn from allearter '.
+			'where Dansk_navn like "%'.$query.'%" and Dansk="Ja"';
+
+		mysql_set_charset('utf8');
+		$result=$this->query($SQL);
+		$json=[];
+		while ($row = mysql_fetch_assoc($result)) {
+			$json[] = $row;
+		}
+		$json = array('allearter' => $json);
+		echo json_encode($json);
+	}
+
+	protected function run_Vn($query) {
+		$SQL='select distinct Videnskabeligt_navn, Dansk_navn from allearter '.
+			'where Videnskabeligt_navn like "%'.$query.'%" and Dansk="Ja"';
+
+		mysql_set_charset('utf8');
+		$result=$this->query($SQL);
+		$json=[];
+		while ($row = mysql_fetch_assoc($result)) {
+			$json[] = $row;
+		}
+		$json = array('allearter' => $json);
+		echo json_encode($json);
+	}
+
 }
 
 ?>
